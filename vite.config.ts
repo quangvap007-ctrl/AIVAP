0ed2b845dd36dff3dc0d077558f5cad582907702
quangvap007-ctrl <quangvap007@gmail.com>
2026-03-14 23:23:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/aivap/',
+    base: '/',
     plugins: [react(), tailwindcss()],
 
     define: {
