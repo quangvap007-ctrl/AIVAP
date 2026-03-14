03d8be3436890e1f9e0f2edca726e0eec0ef235e
quangvap007-ctrl <quangvap007@gmail.com>
2026-03-14 23:13:04 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,38 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+
   return {
-      base: '/aivap/',
+    base: '/aivap/',
     plugins: [react(), tailwindcss()],
+
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
-    server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
+
+    build: {
+      chunkSizeWarningLimit: 1500,   // thêm dòng này để hết warning
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react','react-dom']
+          }
+        }
+      }
     },
+
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+    }
   };
 });
