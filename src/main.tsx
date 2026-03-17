a71ec2639d2ba5025979ac1f9113fef200489910
quangvap007-ctrl <quangvap007@gmail.com>
2026-03-17 08:24:59 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { LanguageProvider } from './contexts/LanguageContext';
-import { ThemeProvider } from './contexts/ThemeContext';
+import { ThemeProvider } from './contexts/ThemeProvider';
+import { SessionProvider } from "next-auth/react"; // Thêm thư viện quản lý phiên đăng nhập
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
+    {/* Bao bọc toàn bộ App bằng SessionProvider để quản lý đăng nhập Google */}
+    <SessionProvider>
+      <ThemeProvider>
         <LanguageProvider>
           <App />
         </LanguageProvider>
-    </ThemeProvider>
+      </ThemeProvider>
+    </SessionProvider>
   </React.StrictMode>
 );
